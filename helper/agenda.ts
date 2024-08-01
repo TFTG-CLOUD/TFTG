665fcf48e1df3a4f4e757d77ad1ccfac34940ea8
TFTG-CLOUD <bted2k@gmail.com>
2024-08-01 23:10:09 +0800

    fix agenda lock job

--- helper/agenda.ts
+++ helper/agenda.ts
@@ -1,7 +1,7 @@
 import { Agenda } from 'agenda';
 import { transcoding } from './ffmpeg';
 
-const agenda = new Agenda({ db: { address: process.env.MONGO_URL! } });
+const agenda = new Agenda({ db: { address: process.env.MONGO_URL! }, lockLimit: 0 });
 
 agenda.on('ready', () => {
   console.log('agenda ready!');
